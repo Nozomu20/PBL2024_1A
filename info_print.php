ece71d0020d3cb29be55aa0c8d457dde9949298a
Nozomu20 <75817855+Nozomu20@users.noreply.github.com>
2024-11-28 20:07:23 +0900

    Organize main

--- info_print.php
+++
@@ -1,9 +0,0 @@
-<?php
-$filename = 'info.txt';
-if (file_exists($filename)) {
-    $content = file_get_contents($filename);
-    echo nl2br(htmlspecialchars($content, ENT_QUOTES, 'UTF-8'));
-} else {
-    echo "お知らせはありません。";
-}
-?>
